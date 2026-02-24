7aea175b8e656bc56c1fa489c19a1f98a7b5702d
John Drefahl <john@velobpa.com>
2026-02-24 05:18:15 -0600

    chore: update .eslintrc.js to v1.0.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,14 @@
-/**
- * @type {import('@types/eslint').ESLint.ConfigData}
+/*
+ * Copyright (c) 2026 Velocity BPA, LLC
+ * Licensed under the Business Source License 1.1
  */
+
 module.exports = {
   root: true,
-  env: {
-    browser: false,
-    es2021: true,
-    node: true,
-    jest: true,
-  },
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: ['./tsconfig.json'],
-    sourceType: 'module',
-    extraFileExtensions: ['.json'],
-  },
-  ignorePatterns: [
-    '.eslintrc.js',
-    'gulpfile.js',
-    'jest.config.js',
-    'node_modules/**',
-    'dist/**',
-    'scripts/**',
-  ],
-  plugins: ['n8n-nodes-base'],
+  plugins: ['eslint-plugin-n8n-nodes-base'],
   extends: ['plugin:n8n-nodes-base/community'],
+  ignorePatterns: ['node_modules/**', 'dist/**'],
   rules: {
     'n8n-nodes-base/community-package-json-name-still-default': 'off',
   },
